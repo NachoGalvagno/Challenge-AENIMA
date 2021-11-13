0aa0083dce3010be66becd2ce27c04b7b3b15a69
NachoGalvagno <ignacio.101@hotmail.com>
2021-11-13 16:58:11 -0300

    tendencias avanzado

--- components/CartaTendencia/CartaTendencia.js
+++ components/CartaTendencia/CartaTendencia.js
@@ -5,6 +5,11 @@ export default function CartaTendencia(){
     return(
         <div className={style.cartaTendencia}>
             <Image src='/Assets/img/card_01.png' alt="carta" width={286} height={180}></Image>
+            <div className={style.textoCarta}>
+                <span className={style.nuevo}>NUEVO</span>
+                <h2 className={style.titulo}>Montañas y magia</h2>
+                <p className={style.subtitulo}>Aquí la descripción para <strong>Montañas y magia</strong></p>
+            </div>
         </div>
     )
 }
